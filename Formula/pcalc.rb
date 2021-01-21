2a3731feaf095f651327f3c8bd6531e7d826cc53
alt-romes <rodrigo.m.mesquita@gmail.com>
2021-01-21 19:21:29 +0000

    Add formula

---
+++ Formula/pcalc.rb
@@ -0,0 +1,21 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class Pcalc < Formula
+  desc "CLI Programming Calculator"
+  homepage ""
+  url "https://github.com/alt-romes/programmer-calculator/archive/v1.0.tar.gz"
+  sha256 "399524be160bf23251a170c949262204d650561b1e58c47af239284b3e3ae1da"
+  license "GNU General Public License v3.0"
+
+  depends_on "cmake" => :build
+  depends_on "ncurses"
+
+  def install
+    system "make", "install"
+    bin.install "pcalc"
+  end
+
+  test do
+    system "true"
+  end
+end
