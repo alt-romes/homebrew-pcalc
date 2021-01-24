1fdfde6cf5016dd899a67abe1a4c4df4bf653dc8
Romes <rodrigo.m.mesquita@gmail.com>
2021-01-24 18:55:19 +0000

    Update pcalc.rb

--- Formula/pcalc.rb
+++ Formula/pcalc.rb
@@ -3,8 +3,8 @@
 class Pcalc < Formula
   desc "CLI Programming Calculator"
   homepage ""
-  url "https://github.com/alt-romes/programmer-calculator/archive/v1.3.tar.gz"
-  sha256 "282a87dcdc848b31a630cf5ce7eb1fd11cc3c6761a4cf7dd04ff81e82f1d045f"
+  url "https://github.com/alt-romes/programmer-calculator/archive/v1.4.tar.gz"
+  sha256 "8fb21d0902cda676944f178a890c8f7302497104202a4e942913871ffb45098f"
   license "GNU General Public License v3.0"
 
   depends_on "cmake" => :build
